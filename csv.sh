d6098f967316acdc703ff46ca5ef50d5d66ee3b2
LFAdams <xlincoln.adamsx@gmail.com>
2019-09-16 11:32:15 -0400

    Update csv.sh

--- csv.sh
+++ csv.sh
@@ -10,8 +10,8 @@ cd $PBS_O_WORKDIR
 module load SAMtools/1.6-foss-2016b
 module load FASTX-Toolkit/0.0.14-foss-2016b
 
-GENOME= /work/cemlab/reference_genomes/97103_v2.fa
-INPUT= /home/lfa81121/blastv1v2/testsnps.csv
+GENOME=/work/cemlab/reference_genomes/97103_v2.fa
+INPUT=/home/lfa81121/blastv1v2/testsnps.csv
 
 OLDIFS=$IFS
 IFS=,
@@ -22,16 +22,16 @@ echo CHROM, POS, REF, ALT, AD_REFLOW, AD_ALTLOW, DPLOW, GQLOW, PLLOW, SNPINDEXLO
 
 while read CHROM POS REF ALT AD_REFLOW AD_ALTLOW DPLOW GQLOW PLLOW SNPINDEXLOW AD_REFHIGH AD_ALTHIGH DPHIGH GQHIGH PLHIGH SNPINDEXHIGH REF_FRQ DELTASNP NSNPS TRICUBEDELTASNP G GPRIME PVALUE NEGLOG10PVAL QVALUE MINDP TRICUBEDP CL95 CL99 SNPLESS250 SNPPLUS250 COORDS
   do
-    SNP1="$(samtools faidx /work/cemlab/reference_genomes/97103_v2.fa $CHROM:$POS-$POS | awk '{if(NR>1)print}')"
-    SNP501="$(samtools faidx /work/cemlab/reference_genomes/97103_v2.fa $CHROM:$SNPLESS250-$SNPPLUS250 | awk '{if(NR>1)print}')"
+    SNP1="$(samtools faidx $GENOME $CHROM:$POS-$POS | awk '{if(NR>1)print}')"
+    SNP501="$(samtools faidx $GENOME $CHROM:$SNPLESS250-$SNPPLUS250 | awk '{if(NR>1)print}')"
     POSLESS23=$((POS-23))
     POSLESS26=$((POS-26))
     POSPLUS23=$((POS+23))
     POSPLUS26=$((POS+26))
-    SNP27="$(samtools faidx /work/cemlab/reference_genomes/97103_v2.fa $CHROM:$POSLESS26-$POS | awk '{if(NR>1)print}')"
-    SNP24="$(samtools faidx /work/cemlab/reference_genomes/97103_v2.fa $CHROM:$POSLESS23-$POS | awk '{if(NR>1)print}')"
-    RSNP27="$(samtools faidx /work/cemlab/reference_genomes/97103_v2.fa $CHROM:$POS-$POSPLUS26 | awk '{if(NR>1)print}' | tr 'gtca' 'cagt' | rev)"
-    RSNP24="$(samtools faidx /work/cemlab/reference_genomes/97103_v2.fa $CHROM:$POS-$POSPLUS23 | awk '{if(NR>1)print}' | tr 'gtca' 'cagt' | rev)"
+    SNP27="$(samtools faidx $GENOME $CHROM:$POSLESS26-$POS | awk '{if(NR>1)print}')"
+    SNP24="$(samtools faidx $GENOME $CHROM:$POSLESS23-$POS | awk '{if(NR>1)print}')"
+    RSNP27="$(samtools faidx $GENOME $CHROM:$POS-$POSPLUS26 | awk '{if(NR>1)print}' | tr 'gtca' 'cagt' | rev)"
+    RSNP24="$(samtools faidx $GENOME $CHROM:$POS-$POSPLUS23 | awk '{if(NR>1)print}' | tr 'gtca' 'cagt' | rev)"
     echo $CHROM, $POS, $REF, $ALT, $AD_REFLOW, $AD_ALTLOW, $DPLOW, $GQLOW, $PLLOW, $SNPINDEXLOW, $AD_REFHIGH, $AD_ALTHIGH, $DPHIGH, $GQHIGH, $PLHIGH, $SNPINDEXHIGH, $REF_FRQ, $DELTASNP, $NSNPS, $TRICUBEDELTASNP, $G, $GPRIME, $PVALUE, $NEGLOG10PVAL, $QVALUE, $MINDP, $TRICUBEDP, $CL95, $CL99, $SNPLESS250, $SNPPLUS250, $COORDS, $SNP1, $SNP501, $SNP27, $SNP24, $RSNP27, $RSNP24 >> /home/lfa81121//blastv1v2/potentialprimers.csv
-
+  done
 IFS=$OLDIFS
